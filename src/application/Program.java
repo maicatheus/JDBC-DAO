64a1d673aeeb735dc8ee3001ab446155a07b3266
maicatheus <maicatheus@gmail.com>
2022-06-10 09:49:26 -0300

    finsByDepartment implementation | change DepName

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,8 +14,16 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== Teste1: seller findById ===");
-		Seller seller = sellerDao.findById(3);
-				
+		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
+		
+		System.out.println("\n\n");
+		System.out.println("=== Teste2: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj : list) {
+			System.out.println();
+			System.out.println(obj);
+		}
 	}
 }
