77e27f48e4987b98462bf43728fea5655ab7cb8b
maicatheus <maicatheus@gmail.com>
2022-06-10 14:35:12 -0300

    Update department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -17,14 +17,21 @@ public class Program2 {
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
 		
-		System.out.println("\n\n=== Teste 2: Department deleteById ===");
+		System.out.println("\n=== Teste 2: Department deleteById ===");
 		departmentDao.deleteById(5);
 		System.out.println("Delete completed!");
 		
-		System.out.println("\n\n=== Teste 3: Department inserrt ===");
+		System.out.println("\n=== Teste 3: Department inserrt ===");
 		Department dep = new Department(null,"Music");
 		departmentDao.insert(dep);
 		System.out.println("Inserted New id: " + dep.getId());
+		
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+		
 		
 		sc.close();
 	}
